db827a00c2c508b85486f37118d2d9f920e56953
momen186 <momenehab186@gmail.com>
2025-07-17 21:45:42 +0300

    initial commit

--- back-end/TalentFlow/settings.py
+++ back-end/TalentFlow/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,12 +78,15 @@ WSGI_APPLICATION = 'TalentFlow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+       config("NEON_CONNECTION_STRING")
+    )
 }
+
+
 
 
 # Password validation
