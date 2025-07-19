af049e8d2feb3350807a638a5acdda0d360406ea
momen186 <momenehab186@gmail.com>
2025-07-19 18:17:56 +0300

    Refactor Employee model and related classes for consistency and clarity

--- back-end/TalentFlow/api/models.py
+++ back-end/TalentFlow/api/models.py
@@ -3,14 +3,19 @@ from django.core.validators import RegexValidator
 
 
 
-class employee(models.Model):
+class Employee(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     phone= models.CharField(max_length=20,validators=[RegexValidator(r'^\+?\d{7,15}$', message="Enter a valid phone number.")])
     address = models.CharField(max_length=255)
     date_joined=models.DateField()
     department = models.ForeignKey('department', on_delete=models.CASCADE)
-    
+    job_title=models.ForeignKey('JobTitle',on_delete=models.PROTECT,related_name='employee')
+    def __str__(self):
+        return self.name
+    class Meta:
+        db_table = 'employee'
+
 class department(models.Model):
     name=models.CharField(max_length=255)
     
@@ -19,21 +24,23 @@ class department(models.Model):
 class JobTitle(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    employee = models.OneToOneField(employee, on_delete=models.CASCADE)
 
 
-class leve_note(models.Model):
+class leaveNote(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     date=models.DateField()
     return_date=models.DateField()
-    employee = models.ForeignKey(employee, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = 'Leave Note'
+        verbose_name_plural = 'Leve Notes'
+
+
     
-    
-class pay_roll(models.Model):
+class PayRoll(models.Model):
     name = models.CharField(max_length=255)
     date=models.DateField()
-    employee = models.ForeignKey(employee, on_delete=models.CASCADE)
     compensation = models.DecimalField(max_digits=10, decimal_places=2)
     net_pay=models.DecimalField(max_digits=10, decimal_places=2)
     gross_pay=models.DecimalField(max_digits=10, decimal_places=2)
