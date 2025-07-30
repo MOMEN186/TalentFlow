83b4006c16086dcd6bbffb96e45d29f6f64854ce
EbrahimDewedar <dewedarrebrahim998@gmail.com>
2025-07-30 20:04:41 +0300

    Resolve merge: auth features + EmployeeDetails route

--- TalentFlow/src/App.jsx
+++ TalentFlow/src/App.jsx
@@ -1,30 +1,38 @@
-// src/App.jsx
 import { Route, Routes } from "react-router-dom";
 import SideBar from "./components/SideBar";
 import Employees from "./pages/Employees";
 import Dashboard from "./pages/Dashboard";
-import PayRoll from "./pages/Payroll";     
+import PayRoll from "./pages/Payroll";
 import NotFound from "./pages/NotFound";
 import LeaveNote from "./pages/LeaveNote";
 import EmployeeDetais from "./pages/EmployeeDetails";
+import Login from "./pages/Login";
+import { AuthProvider } from "./providers/AuthProvider";
+import Profile from "./pages/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
-    <div style={{ display: "flex", height: "100vh" }}>
-      <SideBar />
-      <div style={{ flex: 1, overflowY: "auto" }}>
-        <div className="container my-3">
-          <Routes>
-            <Route path="/" element={<Employees />} />
-            <Route path="/employees" element={<Employees />} />
-            <Route path="/employees/:id" element={<EmployeeDetais />} />
-            <Route path="/payroll" element={<PayRoll/>} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/leave_note" element={<LeaveNote/>} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+    <AuthProvider>
+      <div style={{ display: "flex", height: "100vh" }}>
+        <SideBar />
+        <div style={{ flex: 1, overflowY: "auto" }}>
+          <div className="container my-3">
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/" element={<ProtectedRoute element={<Dashboard />} />} />
+              <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
+              <Route path="/employees" element={<ProtectedRoute element={<Employees />} />} />
+              <Route path="/employees/:id" element={<ProtectedRoute element={<EmployeeDetais />} />} />
+              <Route path="/payroll" element={<ProtectedRoute element={<PayRoll />} />} />
+              <Route path="/leave_note" element={<ProtectedRoute element={<LeaveNote />} />} />
+              <Route path="/profile" element={<ProtectedRoute element={<Profile />} />} />
+              <Route path="*" element={<ProtectedRoute element={<NotFound />} />} />
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+    </AuthProvider>
   );
 }
 
