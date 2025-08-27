9159d93c83e58cb1b25c74d3f1b95fcbdcf8128a
momen186 <momenehab186@gmail.com>
2025-08-27 12:49:24 +0300

    adde docker file

--- back-end/TalentFlow/urls.py
+++ back-end/TalentFlow/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf   import settings
+from django.conf import settings
 from django.conf.urls.static import static
-
-
 
 urlpatterns = [
     path("api/", include("TalentFlow.api.urls")),
@@ -12,9 +10,16 @@ urlpatterns = [
     path("attendance/", include("attendance.urls")),
     path("reports/", include("reports.urls")),
     path("admin/", admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls")),
-    path("silk/", include("silk.urls", namespace="silk")),
 ]
+
 if settings.DEBUG:
+    # Debug only apps
+    import debug_toolbar
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+        path("silk/", include("silk.urls", namespace="silk")),
+    ]
+    
+    # Static + media serving only in development
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
